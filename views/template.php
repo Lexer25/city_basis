d824ce78454eee0ed2a38d3fffaa17fb45582de8
Андрей <andrey.buharov@gmail.com>
2026-08-21 00:43:32 +0300

    Теперь меню не должно закрывать контент

--- views/template.php
+++ views/template.php
@@ -26,29 +26,22 @@
     <link rel="stylesheet" href="/city/static/css/city.css">
     <link rel="stylesheet" href="/city/static/css/2.31.3/theme.blue.min.css">
     <link rel="stylesheet" href="/city/static/css/2.31.3/jquery.tablesorter.pager.min.css">
-	
-	<!-- ДОБАВЬТЕ jQuery UI CSS -->
-    
     <link rel="stylesheet" href="/city/static/css/jquery-ui.css">
-	
-	
     
     <!-- JavaScript -->
     <script src="/city/static/js/jquery-2.2.4.js"></script>
-	
-	<script src="/city/static/js/jquery-ui.min.js"></script>
-	
+    <script src="/city/static/js/jquery-ui.min.js"></script>
     <script src="/city/static/js/moment-with-locales.min.js"></script>
     <script src="/city/static/js/bootstrap.min.js"></script>
     <script src="/city/static/js/bootstrap-datetimepicker.min.js"></script>
     <script src="/city/static/js/2.31.3/jquery.tablesorter.min.js"></script>
     <script src="/city/static/js/2.31.3/jquery.tablesorter.widgets.min.js"></script>
     <script src="/city/static/js/2.31.3/jquery.tablesorter.pager.min.js"></script>
-	
-	
 </head>
 
-<body>
+<!-- ✅ Запасной вариант, если JS не сработает -->
+<body style="padding-top: 200px;">
+
     <!-- Flash-сообщения -->
     <?php if (!empty($flash)): ?>
         <div class="alert <?php echo $flash['class']; ?> alert-dismissible fade in" role="alert">
@@ -65,14 +58,12 @@
         
         <!-- Меню -->
         <?php 
-        // Заменяем оператор ?? на тернарный оператор с isset()
         $menu_html = isset($menu['menu_html']) ? $menu['menu_html'] : '';
         $adm_html = isset($menu['adm_html']) ? $menu['adm_html'] : '';
         $auth_data = isset($auth) ? $auth : array();
         $version_data = isset($version) ? $version : array();
         $odbc_data = isset($odbc) ? $odbc : array();
         $module_info_data = isset($module_info) ? $module_info : array();
-   
 
         echo View::factory('top_menu', array(
             'menu_html' => $menu_html,
@@ -83,12 +74,7 @@
             'module_info' => $module_info_data,
         ))->render(); 
         ?>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        
         <!-- Контент -->
         <?php echo isset($content) ? $content : ''; ?>
         
@@ -128,11 +114,37 @@
             topTime.textContent = bottomTime.textContent;
         }
     });
-    </script>
 
-    <script>
     $(document).ready(function() {
         $('.dropdown-toggle').dropdown();
+        
+        // ✅ АВТОМАТИЧЕСКИЙ РАСЧЁТ ОТСТУПА
+        function fixMenuOffset() {
+            var $menu = $('.navbar-fixed-top');
+            if ($menu.length) {
+                var menuHeight = $menu.outerHeight();
+                if (menuHeight > 0) {
+                    // Добавляем 15px запаса
+                    $('body').css('padding-top', (menuHeight + 15) + 'px');
+                }
+            }
+        }
+        
+        // Вызываем сразу
+        fixMenuOffset();
+        
+        // При изменении размера окна
+        $(window).resize(fixMenuOffset);
+        
+        // После полной загрузки всех элементов (картинки, шрифты и т.д.)
+        $(window).on('load', function() {
+            setTimeout(fixMenuOffset, 100);
+        });
+        
+        // При любых AJAX-запросах
+        $(document).ajaxComplete(function() {
+            setTimeout(fixMenuOffset, 50);
+        });
     });
     </script>
 </body>
